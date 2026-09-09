1a7b831454df1c81b69492e8fd837a791b869a4f
Andreas St <63196263+Andr3as-St@users.noreply.github.com>
2026-09-09 19:16:02 +0300

    Stop logging MongoDB credentials

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const connectDB = (url) => {
-  return mongoose.connect(
-    url,
-    console.info(`Successfully connected to ${process.env.MONGO_URL}`.cyan.underline)
-  );  
+
+const connectDB = async (url) => {
+  await mongoose.connect(url);
+  console.info("Successfully connected to MongoDB".cyan.underline);
 };
+
 export default connectDB;
